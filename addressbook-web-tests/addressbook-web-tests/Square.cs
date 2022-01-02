24160d50f4950fdb645ffc0e63748f8de2c0f886
greiz596 <95532505+greiz596@users.noreply.github.com>
2022-01-02 13:29:13 +0300

    02.01.22
    Создали класс Circle
    Создали класс Square
    Создали общий класс Figure

---
+++ addressbook-web-tests/addressbook-web-tests/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_web_tests
+{  
+    class Square : Figure // :Figure означаетч, что наследует данные от класса Figure и свои собственные имеет
+    {   
+        private int size; //size - поле size (объект. Локальная переменная)
+        private bool colored = false; //квадрат не закрашен
+
+        public Square(int size) // Создание конструктора. Он называется так же, как и класс. Параметр size возвращаемого значения не указывается. Т.к он возвращает объект этого типа. Новый создаваемый объект. Указываются параметры - size. 
+
+        {
+            this.size = size;  //this - ссылка на текущий объект. В его поле помещаем значение, переданное в качестве параметра
+
+        }
+        public int Size
+        { 
+            get 
+            { 
+                return size;
+            } 
+            set
+            {  
+                size = value; 
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/TestSizeRadius.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class TestSizeRadius
+    {
+        [Test]
+        public void Square() //Создаем объекты класса Square в методе Test
+        {
+            //Создал переменные s1, s2, s3 присвоил ссылку на новый объект класса Square. Каждый вызов конструктора создает объект. Дальше можно на него ссылаться из нескольких мест: Передавать его в качестве параметров методы, присваивать переменные. Когда никто больше не ссылается, объект исчезает
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            //проверяем, что размер первого квадрата = 5, второго = 10, третьего = 5
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+            Assert.AreEqual(s1.Size, 15);
+
+            s2.Colored = true;
+
+        }
+
+        [Test]
+        public void Circle() 
+        {
+            Circle s1 = new Circle(5);
+            Circle s2 = new Circle(10);
+            Circle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 5);
+            Assert.AreEqual(s2.Radius, 10);
+            Assert.AreEqual(s3.Radius, 5);
+
+            s3.Radius = 15;
+            Assert.AreEqual(s1.Radius, 15);
+
+            s2.Colored = true;
+        }
+    }
+}
